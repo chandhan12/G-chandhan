8c5fd9b70151f59ab29f2020ec98c43529e5d83c
chandhan <chandhan.g0401@gmail.com>
2025-02-12 11:45:33 +0530

    init

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Sidebar = () => {
+  return (
+    <div className=' h-screen w-72 bg-gray-800 rounded-2xl'>
+     <div>
+        
+     </div>
+    </div>
+  )
+}
+
+export default Sidebar
